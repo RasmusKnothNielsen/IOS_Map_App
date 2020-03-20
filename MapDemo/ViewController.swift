4a63b8f89ab6e025956c11da14909df23a89aae6
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-03-20 08:36:01 +0100

    Initial Commit

---
+++ MapDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapDemo
+//
+//  Created by Rasmus Nielsen on 20/03/2020.
+//  Copyright © 2020 Rasmus Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
